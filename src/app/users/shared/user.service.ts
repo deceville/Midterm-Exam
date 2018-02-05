e93e7cfb7655357e3806adc8bf5c482f392b4860
Decebel Geduriagao <Decebel Geduriagao>
2018-02-05 23:22:26 +0800

    Get observables from Firebase

--- src/app/users/shared/user.service.ts
+++ src/app/users/shared/user.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { FirebaseListObservable, FirebaseObjectObservable, AngularFireDatabase } from 'angularfire2/database';
+import { User } from './user';
 
 @Injectable()
 export class UserService {
 
-  constructor() { }
+	private basePath: string = '/users';
+
+	users: FirebaseListObservable<User[]> = null; //  list of objects
+	user: FirebaseObjectObservable<User> = null; //   single object
+
+	constructor(private af: AngularFire,
+				private db: AngularFireDatabase) { }
+
+
+	getUsersList(query={}): FirebaseListObservable<User[]> {
+	this.users = this.db.list(this.basePath, {
+	query: query
+	});
+	return this.users
+	}
+	// Return a single observable user
+	getItem(key: string): FirebaseObjectObservable<User> {
+	const itemPath =  `${this.basePath}/${key}`;
+	this.user = this.db.object(itemPath)
+	return this.user
+	}
 
 }
