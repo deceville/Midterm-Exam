7f360e10e4c08adc58bcae2da4622b879b49a148
Decebel Geduriagao <Decebel Geduriagao>
2018-02-05 23:19:28 +0800

    Added class User

--- src/app/users/shared/user.ts
+++ src/app/users/shared/user.ts
@@ -1,2 +1,12 @@
 export class User {
+	$key: string
+	firstname: string;
+	lastname: string;
+	email: string;
+	company: string;
+	password: string;
+	phone: string;
+	address: string;
+	timeStamp: number;
+	active: boolean = true;
 }
